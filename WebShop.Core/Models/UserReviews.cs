2058f925594b8eea25194c6370a26773d2bb7cf1
hithahareendran@gmail.com <hithahareendran@gmail.com>
2021-01-18 16:44:29 +0100

    user model created

---
+++ WebShop.Core/Models/UserReviews.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Core.Models
+{
+    public class UserReviews : BaseEntity
+    {
+        
+        public string UserId { get; set; }
+        public string Review { get; set; }
+        public int Rating { get; set; }
+
+    }
+}
