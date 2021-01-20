f8ba9823c1051ece1056e0604d9094a6fa17c536
hithahareendran@gmail.com <hithahareendran@gmail.com>
2021-01-20 20:07:22 +0100

    Adding view model and fixing Carousel

---
+++ WebShop.Core/ViewModels/ProductDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.Core.Models;
+
+namespace WebShop.Core.ViewModels
+{
+    public class ProductDetailsViewModel
+    {
+        public Product product { get; set; }
+        public IEnumerable<Product> relatedProducts { get; set; }
+    }
+}
